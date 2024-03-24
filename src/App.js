fa6c3999aecf96f66fc34f4ba0b0216e2c63011d
dumitru.soltan <dymasoltan@gmail.com>
2024-03-24 18:33:53 +0200

    initial version

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import MovieApp from './components/MovieApp';
+
+function App() {
+    return (
+
+        <div className="App">
+
+            <MovieApp/>
+
+        </div>
+    );
+}
+
+export default App;
